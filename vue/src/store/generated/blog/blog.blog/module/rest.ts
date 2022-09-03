7d51541a8ee3c84914e88e8f4be1a924cb8ba96a
Godswill Adigwe <gcadigwe@gmail.com>
2022-09-03 12:56:57 +0100

    create-comment

--- vue/src/store/generated/blog/blog.blog/module/rest.ts
+++ vue/src/store/generated/blog/blog.blog/module/rest.ts
@@ -23,6 +23,8 @@ export interface BlogComment {
   createdAt?: number;
 }
 
+export type BlogMsgCreateCommentResponse = object;
+
 export interface BlogMsgCreatePostResponse {
   /** @format uint64 */
   id?: string;
@@ -132,13 +134,6 @@ export interface V1Beta1PageRequest {
    * is set.
    */
   count_total?: boolean;
-
-  /**
-   * reverse is set to true if results are to be returned in the descending order.
-   *
-   * Since: cosmos-sdk 0.43
-   */
-  reverse?: boolean;
 }
 
 /**
@@ -368,7 +363,6 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
       "pagination.offset"?: string;
       "pagination.limit"?: string;
       "pagination.count_total"?: boolean;
-      "pagination.reverse"?: boolean;
     },
     params: RequestParams = {},
   ) =>
@@ -426,7 +420,6 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
       "pagination.offset"?: string;
       "pagination.limit"?: string;
       "pagination.count_total"?: boolean;
-      "pagination.reverse"?: boolean;
     },
     params: RequestParams = {},
   ) =>
